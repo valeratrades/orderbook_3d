8895dd51e89a39f26b395678bc26bd740dcc9931
valeratrades <v79166789533@gmail.com>
2024-09-30 20:15:36 +0000

    feat: fuck yes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![feature(trivial_bounds)]
-use std::{str::FromStr, sync::atomic::{AtomicUsize, Ordering}};
+use std::{
+	str::FromStr,
+	sync::atomic::{AtomicUsize, Ordering},
+};
 
 use aggr_orderbook::{BookOrders, ListenBuilder, Market, Symbol};
 use bevy::prelude::*;
@@ -8,7 +11,8 @@ use tokio::sync::mpsc;
 use v_utils::io::{confirm, Percent};
 
 static COUNTER: AtomicUsize = AtomicUsize::new(0);
-
+#[derive(Component)]
+struct MainCamera;
 
 #[tokio::main]
 async fn main() {
@@ -18,7 +22,8 @@ async fn main() {
 		.add_plugins(DefaultPlugins)
 		.add_plugins(EditorPlugin::default())
 		.add_systems(Startup, setup)
-		.add_systems(Update, write_frame).run();
+		.add_systems(Update, write_frame)
+		.run();
 }
 
 #[derive(Debug, Resource)]
@@ -29,13 +34,16 @@ fn write_frame(
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
-	mut camera_query: Query<(&Camera, &mut Transform)>,
+	mut camera_query: Query<(&Camera, &mut Transform), With<MainCamera>>,
 ) {
 	while let Ok(orders) = receiver.0.try_recv() {
 		COUNTER.fetch_add(1, Ordering::Relaxed);
 		let p: v_utils::io::Percent = Percent::from_str("0.02%").unwrap();
 		let (bids, asks) = orders.to_plottable(Some(p), true); //dbg: want aggregate = false
-		dbg!(&bids);
+		if bids.0.is_empty() && asks.0.is_empty() {
+			eprintln!("[WARN] Empty orderbook slice. Consider increasing the depth.");
+			continue;
+		}
 
 		fn to_log(v: Vec<f32>) -> Vec<f32> {
 			let first = v[0]; //NB: smallest value for both, internal promise
@@ -67,7 +75,7 @@ fn write_frame(
 		let (_, mut transform) = camera;
 		transform.translation = Vec3::new(0., 4., range * 1.5 + COUNTER.load(Ordering::SeqCst) as f32);
 
-		confirm("Continue?"); //dbg
+		//confirm("Continue?"); //dbg
 	}
 }
 
@@ -104,19 +112,19 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 		..default()
 	});
 	// camera
-	commands.spawn(Camera3dBundle {
-		transform: Transform::from_xyz(0., 0., 0.).looking_at(Vec3::ZERO, Vec3::Y),
-		..default()
-	});
+	commands.spawn((
+		Camera3dBundle {
+			transform: Transform::from_xyz(0., 0., 0.).looking_at(Vec3::ZERO, Vec3::Y),
+			..default()
+		},
+		MainCamera,
+	));
 
 	std::thread::spawn(move || {
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap();
-    
-    rt.block_on(async {
-        book_listen(tx).await;
-    });
-});
+		let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+
+		rt.block_on(async {
+			book_listen(tx).await;
+		});
+	});
 }
