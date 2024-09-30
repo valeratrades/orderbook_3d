4f8b0ff78399393cf522427536f0a1535f395068
valeratrades <v79166789533@gmail.com>
2024-09-30 19:11:46 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
 #![feature(trivial_bounds)]
-use std::{str::FromStr, sync::Arc};
+use std::str::FromStr;
 
-use aggr_orderbook::{Book, BookOrders, ListenBuilder, Market, Symbol};
+use aggr_orderbook::{BookOrders, ListenBuilder, Market, Symbol};
 use bevy::prelude::*;
 use bevy_editor_pls::prelude::*;
-use tokio::{sync::mpsc, task::JoinSet};
+use tokio::sync::mpsc;
 use v_utils::io::{confirm, Percent};
 
-fn main() {
+static mut COUNTER: usize = 0; // HACK
+
+
+#[tokio::main]
+async fn main() {
 	color_eyre::install().unwrap();
 
 	App::new()
 		.add_plugins(DefaultPlugins)
 		.add_plugins(EditorPlugin::default())
 		.add_systems(Startup, setup)
-		.add_systems(Update, write_frame)
-		.run();
+		.add_systems(Update, write_frame).run();
 }
 
 #[derive(Debug, Resource)]
 struct Receiver(mpsc::Receiver<BookOrders>);
 
-async fn write_frame(
+fn write_frame(
 	mut receiver: ResMut<Receiver>,
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
 	mut camera_query: Query<(&Camera, &mut Transform)>,
 ) {
-
-	while let Some(orders) = receiver.0.recv().await {
+	while let Ok(orders) = receiver.0.try_recv() {
+		// SAFETY: suck it bevy
+		unsafe { COUNTER  += 1 };
 		let p: v_utils::io::Percent = Percent::from_str("0.02%").unwrap();
-		let (bids, asks) = frame.to_plottable(Some(p), false);
+		let (bids, asks) = orders.to_plottable(Some(p), false);
 
 		fn to_log(v: Vec<f32>) -> Vec<f32> {
 			let first = v[0]; //NB: smallest value for both, internal promise
@@ -52,29 +56,23 @@ async fn write_frame(
 			});
 		};
 
-		for i in 0..bids.0.len() {
+		(0..bids.0.len()).for_each(|i| {
 			spawn_object(bids.0[i] - mid_price, bid_qties_log[i], (109.0, 0.97, 0.88));
-		}
-		for i in 0..asks.0.len() {
+		});
+		(0..asks.0.len()).for_each(|i| {
 			spawn_object(asks.0[i] - mid_price, ask_qties_log[i], (0.3, 0.5, 0.5));
-		}
+		});
 
-		//- move the camera back by 1.0
-
-		//HACK: we spawn a new camera, can't update it after
-		//commands.spawn(Camera3dBundle {
-		//	transform: Transform::from_xyz(0., 4., range * 1.5).looking_at(Vec3::ZERO, Vec3::Y),
-		//	..default()
-		//});
 		let camera = camera_query.single_mut();
-		if let Some((_, mut transform)) = camera {
-			transform.translation = Vec3::new(0., 4., range * 1.5);
-		}
+		let (_, mut transform) = camera;
+		// SAFETY: suck it bevy (why is this unsafe, btw?)
+		transform.translation = Vec3::new(0., 4., range * 1.5 + unsafe {COUNTER as f32});
 
 		confirm("Continue?"); //dbg
 	}
 }
 
+//? potentially integrate with async_tasks on bevy or whatever is the most semantically correct way to do this
 async fn book_listen(tx: mpsc::Sender<BookOrders>) {
 	let symbol = Symbol::new(Market::BinancePerp, "BTCUSDT".to_owned());
 	ListenBuilder::new(symbol).data_dir("./examples/data/").listen(tx).await.unwrap();
@@ -112,13 +110,14 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 		..default()
 	});
 
-	std::thread::scope(|s| {
-		s.spawn(|_| {
-			tokio::runtime::Builder::new_multi_thread()
-				.enable_all()
-				.build()
-				.unwrap()
-				.block_on(book_listen(tx));
-		});
-	})
+	std::thread::spawn(move || {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    
+    rt.block_on(async {
+        book_listen(tx).await;
+    });
+});
 }
