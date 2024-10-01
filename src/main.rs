82cb070b3f2e3001bcb153f3b3e3452dfad8b119
valeratrades <v79166789533@gmail.com>
2024-10-01 17:25:03 +0000

    refactor: remove unused deps

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ use std::{
 
 use aggr_orderbook::{BookOrders, ListenBuilder, Market, Symbol};
 use bevy::{color::palettes::css::WHITE, prelude::*, render::camera::PerspectiveProjection};
-use bevy_editor_pls::prelude::*;
+use bevy::input::common_conditions::input_just_pressed;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
-use iyes_perf_ui::entries::PerfUiBundle;
 use tokio::sync::mpsc;
 use v_utils::io::Percent;
 
-static COUNTER: AtomicUsize = AtomicUsize::new(0);
-//#[derive(Component)]
-//struct MainCamera;
+static N_ROWS_DRAWN: AtomicUsize = AtomicUsize::new(0);
 
 #[tokio::main]
 async fn main() {
@@ -22,56 +19,66 @@ async fn main() {
 
 	App::new()
 		.add_plugins(DefaultPlugins)
-		.add_plugins(EditorPlugin::default())
 		.add_plugins(PanOrbitCameraPlugin)
 		.add_systems(Startup, setup)
 		.add_systems(Update, write_frame)
+		.add_systems(Update, center_camera.run_if(input_just_pressed(KeyCode::Escape)))
 		.run();
 }
 
 #[derive(Debug, Resource)]
-struct Receiver(mpsc::Receiver<BookOrders>);
+struct Shared{
+	receiver: mpsc::Receiver<BookOrders>,
+	asks_material_handle: Handle<StandardMaterial>,
+	bids_material_handle: Handle<StandardMaterial>,
+	cuboid_mesh_handle: Handle<Mesh>,
+}
+
+fn center_camera(mut camera_query: Query<(&Camera, &mut Transform), With<PanOrbitCamera>>) {
+	let camera = camera_query.single_mut();
+	let (_, mut transform) = camera;
+	transform.translation = Vec3::new(0., 4., 0.);
+}
 
 fn write_frame(
-	mut receiver: ResMut<Receiver>,
+	mut shared: ResMut<Shared>,
 	mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>,
 	mut camera_query: Query<(&Camera, &mut Transform), With<PanOrbitCamera>>,
 ) {
-	while let Ok(orders) = receiver.0.try_recv() {
+	while let Ok(orders) = shared.receiver.try_recv() {
 		let p: v_utils::io::Percent = Percent::from_str("0.02%").unwrap();
 		let (bids, asks) = orders.to_plottable(Some(p), true, true);
 		if bids.0.is_empty() && asks.0.is_empty() {
 			eprintln!("[WARN] Empty orderbook slice. Consider increasing the depth.");
 			continue;
 		}
-		COUNTER.fetch_add(1, Ordering::Relaxed);
+		N_ROWS_DRAWN.fetch_add(1, Ordering::Relaxed);
 
 		let mid_price = (bids.0[0] + asks.0[0]) / 2.;
 		let range = asks.0[asks.0.len() - 1] - bids.0[bids.0.len() - 1];
 
-		let mesh_handle = meshes.add(Cuboid::new(range / 1000., 1.0, 1.0));
-
-		let mut spawn_object = |x: f32, y: f32, hsl: (f32, f32, f32)| {
+		let mut spawn_object = |x: f32, y: f32, material_handle: Handle<StandardMaterial> | {
 			commands.spawn(PbrBundle {
-				mesh: mesh_handle.clone(),
-				material: materials.add(Color::hsl(hsl.0, hsl.1, hsl.2)),
-				transform: Transform::from_xyz(x, y / 2., COUNTER.load(Ordering::SeqCst) as f32).with_scale(Vec3::new(1., y, 1.)),
+				mesh: shared.cuboid_mesh_handle.clone(),
+				material: material_handle,
+				transform: Transform::from_xyz(x, y / 2., N_ROWS_DRAWN.load(Ordering::SeqCst) as f32).with_scale(Vec3::new(range / 1000., y, 1.)), //HACK: x scale could be shared
 				..default()
 			});
 		};
 
 		(0..bids.0.len()).for_each(|i| {
-			spawn_object(bids.0[i] - mid_price, bids.1[i], (109.0, 0.97, 0.88));
+			spawn_object(bids.0[i] - mid_price, bids.1[i], shared.bids_material_handle.clone());
 		});
 		(0..asks.0.len()).for_each(|i| {
-			spawn_object(asks.0[i] - mid_price, asks.1[i], (0.3, 0.5, 0.5));
+			spawn_object(asks.0[i] - mid_price, asks.1[i], shared.asks_material_handle.clone());
 		});
 
 		let camera = camera_query.single_mut();
 		let (_, mut transform) = camera;
-		transform.translation = Vec3::new(0., 4., range * 1.5 + COUNTER.load(Ordering::SeqCst) as f32);
+		let current_camera_pos = transform.translation;
+		if current_camera_pos.x == 0. {
+			transform.translation = Vec3::new(0., 4., range * 1.5 + N_ROWS_DRAWN.load(Ordering::SeqCst) as f32);
+		}
 	}
 }
 
@@ -84,10 +91,13 @@ async fn book_listen(tx: mpsc::Sender<BookOrders>) {
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
 	let (tx, rx) = mpsc::channel(65536);
-	commands.insert_resource(Receiver(rx));
-
-	// perf debug
-	commands.spawn(PerfUiBundle::default());
+	let shared = Shared {
+		receiver: rx,
+		asks_material_handle: materials.add(Color::hsl(0.3, 0.5, 0.5)),
+		bids_material_handle: materials.add(Color::hsl(109.0, 0.97, 0.88)),
+		cuboid_mesh_handle: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+	};
+	commands.insert_resource(shared);
 
 	commands.insert_resource(AmbientLight {
 		color: WHITE.into(),
@@ -98,7 +108,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 	commands.spawn((
 		Camera3dBundle {
 			transform: Transform::from_xyz(0., 0., 0.).looking_at(Vec3::ZERO, Vec3::Y),
-			projection: PerspectiveProjection { far: 100.0, ..default() }.into(),
+			projection: PerspectiveProjection { far: 1000.0, ..default() }.into(),
 			..default()
 		},
 		PanOrbitCamera::default(),
